cc4f3e5574c660c676449afe8a060dd781ddf631
Anderson Araujo <666anderson.araujo@gmail.com>
2016-11-24 15:29:11 -0200

    commit do projeto inicial

---
+++ ProjetoFinalLogica/src/estoque/NotificaWhatsapp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estoque;
+
+import domain.Notificacao;
+
+
+public class NotificaWhatsapp extends Notificacao{
+    
+    public NotificaWhatsapp(String mensagem) {
+            super(mensagem);
+    }
+
+    public void enviar() {
+            System.out.println("Enviando notificacao por Whatsapp...");
+            System.out.println(getMensagem());
+    }
+}
---
+++ ProjetoFinalLogica/src/fastfood/NotaFiscalBuilderFastFood.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fastfood;
+
+import domain.Demanda;
+import domain.NotaFiscal;
+import domain.NotaFiscalBuilder;
+import java.util.Date;
+import java.util.List;
+
+
+public class NotaFiscalBuilderFastFood implements NotaFiscalBuilder{
+
+    private String nomeEmpresa;
+    private String nomeCliente;
+    private Long codigo;
+    private Date dataFaturamento;
+    private double valorTotal;
+    private List<Demanda> demandas;
+
+    @Override
+    public void buildCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    @Override
+    public void buildEmpresa(String nomeEmpresa) {
+        this.nomeEmpresa = nomeEmpresa;
+    }
+
+    @Override
+    public void buildCodigoPedido(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public void buildDataFaturamento(Date dataFaturamento) {
+        this.dataFaturamento = dataFaturamento;
+    }
+
+    @Override
+    public void buildDemandas(List<Demanda> demandas) {
+        this.demandas = demandas;
+    }
+
+    @Override
+    public void buildValorTotal(double valor) {
+        this.valorTotal = valor;
+    }
+
+    @Override
+    public NotaFiscal getComprovante() {
+        return new NotaFiscalFastFood(nomeEmpresa, nomeCliente, codigo, dataFaturamento, valorTotal, demandas);
+    }
+    
+}
